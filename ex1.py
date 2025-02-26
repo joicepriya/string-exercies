7f2708203504da95e5cff1d7ccc49c5657a9631b
joicepriya <joicepriya796@gmail.com>
2025-02-26 13:22:49 +0530

    I uploaded feb 26 1:22 pm

---
+++ ex1.py
@@ -0,0 +1,23 @@
+#sting methods
+
+
+text=input("enter the text:")
+print(text.capitalize())
+print(text.casefold())
+print(text.center(30,'_'))
+print(text.count("J"))
+print(text.encode())
+print("joice".endswith("ce"))
+print("joice\t priya".expandtabs(10))
+print("gladyajoshpin".find("joshpin"))
+print(text.format())
+print(text.index(""))
+print("angel2502".isalnum())
+print("170624".isdecimal())
+print("5695".isdigit())
+print("it is python variable".isidentifier())
+print(text.islower())
+print(text.isupper())
+print("123.567".isnumeric())
+print("jerusha\n".isprintable())
+
